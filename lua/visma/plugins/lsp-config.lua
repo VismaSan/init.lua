c557c3de25879fb610f63bdb3ac9ba1750288c7e
Vismantas Sanda <vismantas.sanda@gmail.com>
2024-05-17 23:42:05 +0300

    include csharp lsp

--- lua/visma/plugins/lsp-config.lua
+++ lua/visma/plugins/lsp-config.lua
@@ -7,23 +7,34 @@ return {
         end
     },
     {
+        -- LSP config: installing language servers on machine
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
+                ensure_installed = { "lua_ls", "csharp_ls" }
             })
         end
     },
     {
+        -- Communication from nvim to LSP: setup all languages separately
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
+
             lspconfig.lua_ls.setup({})
+            lspconfig.csharp_ls.setup({})
+
+            -- :h vim.lsp.buf -> command to show all available lsp actions
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+
+            --vim.keymap.set('n', 'D', vim.lsp.buf.type_definition, {}) -> open in floating view not new buf
+            -- vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, {})
+            -- references({context}, {options})
+            -- type_definition({options})
         end
     }
 }
