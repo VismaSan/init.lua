5701a8824f42e79a8958f0d205eb85132720a729
Vismantas Sanda <vismantas.sanda@gmail.com>
2024-05-24 23:32:32 +0300

    include vim-surround

---
+++ lua/visma/plugins/vim-surround.lua
@@ -0,0 +1,11 @@
+return {
+    "tpope/vim-surround"
+
+    -- how to use:
+    --
+    -- cs"' -> changes "this" to 'this'
+    -- cs'<p> -> changes 'this' to <p>this</p>
+    -- cst" -> changes <p>this</p> to "this"
+    --
+    -- ds" -> removes "" from "this"
+}
