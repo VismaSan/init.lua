4f175882750a2ce527dc3ec889a351b3cbebccc3
Visma <vismantas.sanda@gmail.com>
2026-04-07 09:26:24 +0000

    show gitignored files in solution view

--- lua/visma/plugins/nvim-tree.lua
+++ lua/visma/plugins/nvim-tree.lua
@@ -1,21 +1,27 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    vim.keymap.set("n", "<localleader>t", ":NvimTreeToggle<CR>")
-    vim.keymap.set("n", "<localleader>f", ":NvimTreeFocus<CR>")
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        vim.keymap.set("n", "<localleader>t", ":NvimTreeToggle<CR>")
+        vim.keymap.set("n", "<localleader>f", ":NvimTreeFocus<CR>")
 
-    require("nvim-tree").setup {
-        view = {
-            adaptive_size = true
+        require("nvim-tree").setup {
+            view = {
+                adaptive_size = true
+            },
+            filters = {
+                dotfiles = true
+            },
+            git = {
+                ignore = false
+            }
         }
-    }
-    require("nvim-web-devicons").setup {
-        default = true
-    }
-  end
+        require("nvim-web-devicons").setup {
+            default = true
+        }
+    end
 }
